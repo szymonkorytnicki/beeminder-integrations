931ff19bb7420f3c5fb2c3feae1d84aaa0422863
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2021-11-04 21:24:32 +0100

    NONE fix github integration

--- github-has-commit/utils.js
+++ github-has-commit/utils.js
@@ -1,7 +1,10 @@
 const today = new Date();
 const pad = (int) => (int + "").padStart(2, "0");
-let todayInBeeminderFormat =
-  today.getFullYear() + pad(today.getMonth() + 1) + pad(today.getDate());
+let todayInBeeminderFormat = [
+  today.getFullYear(),
+  pad(today.getMonth() + 1),
+  pad(today.getDate()),
+].join("-");
 
 module.exports = {
   hasCommitToday: function (feed) {
